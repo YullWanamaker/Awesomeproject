0164a44ac26c9cdcd70038bce7961e6fbe42e7f1
Wanamaker <dla4038@naver.com>
2022-08-24 18:30:15 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,33 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, ScrollView } from "react-native-web";
 
 export default function App() {
   return (
-    <View
-      style={{
-        height: 200,
-        padding: 50,
-      }}
-    >
-      <Text
-        style={{
-          fontSize: 30,
-          fontWeight: "900",
-        }}
-      >
-        Hello World!
-      </Text>
-      <Image
-        style={{
-          width: 200,
-          height: 200,
-        }}
-        source={{
-          uri: "https://s2.coinmarketcap.com/static/img/coins/64x64/1698.png",
-        }}
-      />
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <view style={{ height: 300 }}></view>
+        <view style={{ heigt: 300 }}>
+          <Textinput
+            style={{
+              borderWidth: 1,
+              padding: 10,
+            }}
+            placeholder="메뉴입력"
+          />
+        </view>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
